a7faa9fc6604343e49f6c89f9d7fc50ae12d05d0
rja907 <kmrrajj@gmail.com>
2018-02-16 19:10:31 +0530

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'build'), //'__' current working directory.
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/ //babel will be applied to files that end with .js.
+      }
+    ]
   }
 };
 
